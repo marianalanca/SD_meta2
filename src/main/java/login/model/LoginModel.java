d745bc5d8d5faf32e60ee515559df8a950da9960
marianalanca <mariana-lanca@hotmail.com>
2021-05-02 19:59:48 +0100

    change

---
+++ src/main/java/login/model/LoginModel.java
@@ -0,0 +1,12 @@
+package login.model;
+
+import java.util.ArrayList;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
+import RMIServer_I; // RMI SERVER
+
+public class LoginModel {
+
+}
